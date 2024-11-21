3877cae64b5d3b7f0d2d1ed54087ec8964e69098
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-11-21 08:56:44 -0800

    Added an umbrella header.

--- include/tatami_test/tatami_test.hpp
+++ include/tatami_test/tatami_test.hpp
@@ -1,11 +1,24 @@
-#ifndef TATAMI_TEST_HPP
-#define TATAMI_TEST_HPP
+#ifndef TATAMI_TEST_TATAMI_TEST_HPP
+#define TATAMI_TEST_TATAMI_TEST_HPP
 
-#include "simulate_vector.hpp"
-#include "test_access.hpp"
-#include "test_unsorted.hpp"
 #include "fetch.hpp"
+#include "ForcedOracleWrapper.hpp"
+#include "ReversedIndicesWrapper.hpp"
+#include "simulate_vector.hpp"
+#include "simulate_compressed_sparse.hpp"
+#include "test_access.hpp"
+#include "test_unsorted_access.hpp"
 #include "throw_error.hpp"
-#include "TestWrapper.hpp"
+
+/**
+ * @file tatami_test.hpp
+ * @brief Umbrella header for **tatami_test**.
+ */
+
+/**
+ * @namespace tatami_test
+ * @brief Utilities for testing **tatami** libraries.
+ */
+namespace tatami_test {}
 
 #endif
